94158aebad09670c34917efb77fd770956e084d3
Chris Yip <i@chrisyip.im>
2015-04-21 14:28:34 +0800

    Make output more readable

--- run.sh
+++ run.sh
@@ -1,15 +1,18 @@
 node_version=`node -v`
 echo "Using node $node_version"
 echo "Current NODE_PATH is '$NODE_PATH'"
-echo "Executing 'node foo/bar.js', you should seeing an error...\n"
+echo "Executing 'node foo/bar.js', you should seeing an error..."
+echo ""
 
 node foo/bar.js || true
 
-echo "\nAppeding a trailing colon to NODE_PATH"
+echo ""
+echo "Appeding a trailing colon to NODE_PATH"
 
 export NODE_PATH=$NODE_PATH:
 
 echo "NODE_PATH now is '$NODE_PATH' <- notice the colon in the end"
-echo "Executing 'node foo/bar.js'...\n"
+echo "Executing 'node foo/bar.js'..."
+echo ""
 
 node foo/bar.js
